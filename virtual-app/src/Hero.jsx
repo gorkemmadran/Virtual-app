8dc88b72bf6f49972dd67fb4ecbc9c052a737c81
Gorkem Madran <gorkemmadran@hotmail.com>
2024-08-13 13:28:48 +0300

    Full responsive hero section

--- virtual-app/src/Hero.jsx
+++ virtual-app/src/Hero.jsx
@@ -6,11 +6,11 @@ export default function Hero() {
   return (
     <main className='pt-24 w-full bg-primary text-white h-screen flex justify-center'>
       <section>
-        <h1 className='text-6xl text-center'>
+        <h1 className='text-6xl xs:text-5xl sm:text-5xl  md:text-5xl  lg:text-6xl  xl:text-6xl 2xl:text-6xl text-center'>
           VirtualR build tools <span className='bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-orange-900'>for</span><br/>
           <span className='bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-orange-900'>developers</span>
         </h1>
-        <p className='pt-7 text-center text-gray-500'>
+        <p className='xs:p-7 sm:p-6 md:p-0 lg:p-0 xl:p-0 2xl:p-0 mt-4  text-center text-gray-500'>
           Empower your creativity and bring your VR app ideas to life with our intuitive development tools.<br/>
           Get started today and turn your imagination into immersive reality!
         </p>
@@ -23,10 +23,10 @@ export default function Hero() {
           </button>
         </div>
         <div className='flex items-center justify-center pt-10'>
-          <video autoPlay loop muted  className='rounded-lg w-1/3 border border-orange-700 shadow-orange-400 mx-2  my-4'>
+          <video autoPlay loop muted  className='rounded-lg xs:w-52 sm:w-72 md:w-96 lg:w-1/3 xl:w-1/3 2xl:w-1/3  border border-orange-700 shadow-orange-400 mx-2  my-4'>
             <source type='video/mp4' src={Herovideo1}></source>
           </video>
-          <video autoPlay loop muted  className='rounded-lg w-1/3  border border-orange-700 shadow-orange-400 mx-2  my-4'>
+          <video autoPlay loop muted  className='rounded-lg xs:w-52 sm:w-72 md:w-96 lg:w-1/3 xl:w-1/3 2xl:w-1/3 border border-orange-700 shadow-orange-400 mx-2  my-4'>
             <source type='video/mp4' src={Herovideo2}></source>
           </video>
         </div>
